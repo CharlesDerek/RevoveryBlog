be90c101f87b713f063c21fbe2fae7525f2eceed
Charles Derek <charlesderek177@gmail.com>
2018-02-02 20:40:02 +0000

    Add categories model and tests

--- alpha_blog/app/controllers/articles_controller.rb
+++ alpha_blog/app/controllers/articles_controller.rb
@@ -18,9 +18,9 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         @article.user = current_user
         if @article.save
-            flash[:success] = "Article was successfully created"
-        redirect_to article_path(@article)
-    else 
+          flash[:success] = "Article was successfully created"
+          redirect_to article_path(@article)
+        else 
         render 'new'
         end
     end 
